5ae91cb4f354a2475d2bd67ab25f53595d005cf3
Parchello <parchellos@gmail.com>
2023-11-25 13:45:41 +0200

    finished

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import { SearchBar } from './SearchBar/SearchBar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
@@ -6,81 +6,65 @@ import { Loader } from './Button/Loader';
 import { fetchImages } from './api';
 import { MagnifyingGlass } from 'react-loader-spinner';
 
-export class App extends Component {
-  state = {
-    images: [],
-    isLoading: false,
-    query: '',
-    page: 1,
-    showBtn: false,
-  };
+export const App = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [showBtn, setShowBtn] = useState(false);
 
-  onSearch = async newQuery => {
+  const onSearch = async newQuery => {
     if (newQuery === '') {
       toast.error('Please enter something to search');
     } else {
-      this.setState({
-        isLoading: true,
-        query: newQuery,
-        page: 1,
-        images: [],
-      });
+      setIsLoading(true);
+      setQuery(newQuery);
+      setPage(1);
+      setImages([]);
     }
   };
 
-  componentDidUpdate(prevProps, prevState) {
-    if (
-      prevState.query !== this.state.query ||
-      prevState.page !== this.state.page
-    ) {
-      this.fetchUpdatedImages();
-    }
-  }
+  useEffect(() => {
+    if (query === '') return;
+    const fetchUpdatedImages = async () => {
+      try {
+        setIsLoading(true);
+        const searchedImages = await fetchImages(query, page);
 
-  fetchUpdatedImages = async () => {
-    const { page, query } = this.state;
+        setImages(prevImages => [...prevImages, ...searchedImages.hits]);
+        setShowBtn(page < Math.ceil(searchedImages.totalHits / 12));
+        setIsLoading(false);
+      } catch (error) {
+        toast.error('There is an error fetching images');
+        setIsLoading(false);
+      }
+    };
 
-    try {
-      this.setState({ isLoading: true });
-      const searchedImages = await fetchImages(query, page);
+    fetchUpdatedImages();
+  }, [query, page]);
 
-      this.setState(prevState => ({
-        images: [...prevState.images, ...searchedImages.hits],
-        showBtn: this.state.page < Math.ceil(searchedImages.totalHits / 12),
-        isLoading: false,
-      }));
-    } catch (error) {
-      toast.error('There is an error fetching images');
-      this.setState({ isLoading: false });
-    }
+  const onAdd = () => {
+    setPage(prevPage => prevPage + 1);
   };
 
-  onAdd = () => {
-    this.setState(prevState => {
-      return { page: prevState.page + 1 };
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <SearchBar addGalery={this.onSearch} />
-        {this.state.isLoading && (
-          <MagnifyingGlass
-            visible={true}
-            height="80"
-            width="80"
-            ariaLabel="MagnifyingGlass-loading"
-            wrapperStyle={{}}
-            wrapperClass="MagnifyingGlass-wrapper"
-            glassColor="#c0efff"
-            color="#e15b64"
-          />
-        )}
-        <ImageGallery imagesRender={this.state.images} />
-        {this.state.showBtn && <Loader addPage={this.onAdd} />}
-        <Toaster />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <SearchBar addGalery={onSearch} />
+      {isLoading && (
+        <MagnifyingGlass
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="MagnifyingGlass-loading"
+          wrapperStyle={{}}
+          wrapperClass="MagnifyingGlass-wrapper"
+          glassColor="#c0efff"
+          color="#e15b64"
+        />
+      )}
+      <ImageGallery imagesRender={images} />
+      {showBtn && <Loader addPage={onAdd} />}
+      <Toaster />
+    </div>
+  );
+};
